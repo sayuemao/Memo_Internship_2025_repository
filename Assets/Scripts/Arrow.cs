0e0fe734f2cc74c0456cf0960019215c72df5ea8
sayuemao <ynr040ddg@foxmail.com>
2025-10-04 22:09:55 +0800

    继续写箭头逻辑

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -14,6 +14,12 @@ public class Arrow : MonoBehaviour
     public float stayTime = 7.0f;
     private float staycounter ;
 
+    public float startFlickerTime = 3.5f;  // 开始闪烁的时间点，是剩余的时间
+    public float flickerInterval = 0.5f; // 初始闪烁间隔时间
+    private float currentFlickerTime;
+
+    private float nextFlickerTime; // 下次闪烁的时间点
+
     public float deadTime = 15.0f;
     private float deadcounter ;
 
@@ -21,18 +27,27 @@ public class Arrow : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer SR;
+    private BoxCollider2D boxCollider2d;
+ 
+
+    private float rayExtra = 0.05f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         SR = GetComponent<SpriteRenderer>();
+        SR.enabled = true;
+        boxCollider2d = GetComponent<BoxCollider2D>();
         platform = transform.GetChild(0).gameObject;
         platform.SetActive(false);
+
         staycounter = -1f;
         deadcounter = deadTime;
+        currentFlickerTime = 0;
+        nextFlickerTime = flickerInterval+Time.time;
     }
 
-    
-    void Update()
+    private void FixedUpdate()
     {
         Vector2 destination;
         if (transform.localScale.x > 0)//向左
@@ -44,16 +59,19 @@ public class Arrow : MonoBehaviour
             destination = Vector2.right;
         }
 
-        RaycastHit2D hit = Physics2D.Raycast(detectPoint.transform.position, destination, 0.1f, detectLayer);
+        float castDist = rb.velocity.magnitude * Time.fixedDeltaTime + rayExtra;
+
+        // 如果速度很小也给一点最小检测距离
+        if (castDist < rayExtra) castDist = rayExtra;
+
+        RaycastHit2D hit = Physics2D.Raycast(detectPoint.transform.position, destination, castDist, detectLayer);
 
         if (hit.collider != null)
         {
             shouldBePlatform = true;
         }
 
-        //shouldBePlatform = Physics2D.OverlapCircle(detectPoint.transform.position, 0.2f, detectLayer);  
-
-        if(shouldBePlatform)
+        if (shouldBePlatform)
         {
             if (!platform.activeSelf)
             {
@@ -63,13 +81,16 @@ public class Arrow : MonoBehaviour
                 rb.isKinematic = true;
 
                 SR.sortingLayerName = "Map";
-                SR.sortingOrder = -1;
+                SR.sortingOrder = 0;
+                
+                boxCollider2d.enabled = false;
 
-                gameObject.layer = LayerMask.NameToLayer("ArrowPlatform");
-            }                    
+                platform.layer = LayerMask.NameToLayer("ArrowPlatform");
+            }
         }
-
-
+    }
+    void Update()
+    {
         if (staycounter > 0)
         {
             staycounter -= Time.deltaTime;
@@ -79,7 +100,6 @@ public class Arrow : MonoBehaviour
             }
         }
         
-
         if(deadcounter>0)
         {
             deadcounter -= Time.deltaTime;
@@ -89,10 +109,27 @@ public class Arrow : MonoBehaviour
             }
         }
 
+        /*当剩余时间小于等于设定的开始闪烁时间时，开始计算闪烁间隔,闪烁间隔会随着剩余时间减少而缩短，公式为0.1f + (remainingTime / startFlickerTime) * 0.4f，确保即使在最后时刻也有基本的闪烁间隔
+        通过启用 / 禁用物体的Renderer组件来实现闪烁效果*/
+        if ((shouldBePlatform && staycounter < startFlickerTime)||(deadcounter<startFlickerTime))
+        {
+            currentFlickerTime += Time.deltaTime;
+            float remainingFlickerTime = startFlickerTime - currentFlickerTime;
+            flickerInterval = 0.1f + (remainingFlickerTime / startFlickerTime) * 0.4f;
+            if (Time.time >= nextFlickerTime)
+            {
+                SR.color = new Color(SR.color.r,SR.color.g,SR.color.b,(SR.color.a==1f)?0.5f:1f); // 切换透明度实现闪烁
+                nextFlickerTime = Time.time + flickerInterval;
+            }
+        }
+        
     }
 
     private void DestroyArrow()
     {
         Destroy(gameObject);
     }
+
+
+
 }
